31ae43dd0dcba830912675336b9e2fcf1f7e61fa
Joshua Kidd <kidd.josh.343@gmail.com>
2016-02-07 05:33:57 -0700

    Added most of the CSS and HTML from feat/front_end_design branch

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -15,15 +15,22 @@ defmodule Labs.PostController do
     query = from p in Post,
       where: p.slug == ^slug,
       preload: [:user]
-      
+
     render conn, "show.html",
       post: Repo.one query
   end
 
   def new(conn, _) do
+    render conn, "new.html"
   end
 
   def create(conn, %{"post" => post_params}) do
+    # post = Post.changeset %Post{}, post_params
+    # |> put_change(:user_id, get_session(conn, :current_user))
+    #
+    # case Repo.insert post do
+    #   {:ok, post} ->
+    # end
   end
 
   def edit(conn, %{"post" => post_params}) do
--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -4,7 +4,7 @@ defmodule Labs.SessionController do
   alias Comeonin.Bcrypt
 
   plug Labs.Plugs.Admin, "layout" when action in [:new]
-  
+
   plug :scrub_params, "user" when action in [:create]
 
   def new(conn, _) do
